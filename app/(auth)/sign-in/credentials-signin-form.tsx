c3338a781839436e8ecfbd5b340a8116ca0d0ed4
Tatiana Grigorieva <tsg1204@gmail.com>
2025-01-07 01:07:19 -0500

    Sign in page added

---
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { signInDefaultValues } from '@/lib/constants';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
+import { signInWithCredentials } from '@/lib/actions/user.actions';
+
+const CredentialsSigninForm = () => {
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: '',
+  });
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+      <Button disabled={pending} className="w-full" variant="default">
+        {pending ? 'Signing In...' : 'Sign In'}
+      </Button>
+    );
+  };
+
+  return (
+    <form action={action}>
+      <div className="space-y-6">
+        <div>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            required
+            autoComplete="email"
+            defaultValue={signInDefaultValues.email}
+          />
+        </div>
+        <div>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            required
+            autoComplete="password"
+            defaultValue={signInDefaultValues.password}
+          />
+        </div>
+        <div>
+          <SignInButton />
+        </div>
+        {data && !data.success && (
+          <div className="text-center text-destructive">{data.message}</div>
+        )}
+        <div className="text-sm text-center text-muted-forground">
+          Don&apos;t have an account?{' '}
+          <Link href="/sign-up" target="_self" className="link">
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default CredentialsSigninForm;
