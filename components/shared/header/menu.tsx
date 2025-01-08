41777f3a329e302323525f7cd101ec81b8d44591
Tatiana Grigorieva <tsg1204@gmail.com>
2025-01-08 12:20:02 -0500

    UserIcon error fixed

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { Button } from '../../ui/button';
 import ModeToggle from './mode-toggle';
-import { EllipsisVertical, ShoppingCart, UserIcon } from 'lucide-react';
+import { EllipsisVertical, ShoppingCart } from 'lucide-react';
 import {
   Sheet,
   SheetContent,
